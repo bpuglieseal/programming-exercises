ac7dc133900dfa8882abaf05b612e747706d6486
Baldassare Pugliese <bpuglieseal24dw@ikzubirimanteo.com>
2024-11-14 10:33:58 +0100

    Realizando los ejercicios pendientes de arrays

--- Arrays/src/Exercise13.java
+++ Arrays/src/Exercise13.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Exercise13 {
     public static void print_array(int[] array) {
         System.out.print("[");
@@ -8,12 +10,28 @@ public class Exercise13 {
         System.out.print("]\n");
     }
 
-    public static int[] mix (int[] array, int[] array2) {
-        int[] new_array = new int[array.length + array2.length];
-        for (int i = 0, j = 0; j < array.length; i += 2, j++) {
-            new_array[i] = array[j];
-            new_array[i + 1] = array2[j];
+    public static int[] mix (int[] first_array, int[] second_array) {
+        int[] new_array = new int[first_array.length + second_array.length];
+        for (int i = 0, j = 0, aux = 0; aux < new_array.length;) {
+            if (i == first_array.length) {
+                new_array[aux] = second_array[j];
+                aux++;
+                j++;
+            } else if (j == second_array.length) {
+                new_array[aux] = first_array[i];
+                aux++;
+                i++;
+            } else if (first_array[i] <= second_array[j]) {
+                new_array[aux] = first_array[i];
+                i++;
+                aux++;
+            } else if (second_array[j] <= first_array[i]) {
+                new_array[aux] = second_array[j];
+                j++;
+                aux++;
+            }
         }
+
         return new_array;
     }
 
@@ -33,10 +51,29 @@ public class Exercise13 {
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 4, 9, 20};
-        int[] array2 = {2, 59, 70, 21, -1, 56};
-        array = mix(array, array2);
-        sort(array);
-        print_array(array);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input the N size of the two arrays: ");
+        int n = scan.nextInt();
+
+        int[] first_array = new int[n];
+        int[] second_array = new int[n];
+
+        for (int i = 0; i < first_array.length; i++) {
+            System.out.format("Input the element %d of the first array: ", i + 1);
+            first_array[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < first_array.length; i++) {
+            System.out.format("Input the element %d of the second array: ", i + 1);
+            second_array[i] = scan.nextInt();
+        }
+
+        sort(first_array);
+        sort(second_array);
+
+        first_array = mix(first_array, second_array);
+
+        System.out.print("Result array: ");
+        print_array(first_array);
     }
 }
