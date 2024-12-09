94a0634b520bfb2f2ba313c74b5a8bdc8a4397f7
Baldassare Pugliese <bpuglieseal24dw@ikzubirimanteo.com>
2024-12-09 08:51:40 +0100

    Haciendo el ejercicio 2

---
+++ POO/src/Excercise2/Main.java
@@ -0,0 +1,8 @@
+package Excercise2;
+
+public class Main {
+    public static void main(String[] args) {
+        Multiplier m = new Multiplier(10);
+        System.out.format("%d", m.multiply(5));
+    }
+}
---
+++ POO/src/Excercise2/Multiplier.java
@@ -0,0 +1,13 @@
+package Excercise2;
+
+public class Multiplier {
+    private int number;
+
+    Multiplier (int number) {
+        this.number = number;
+    }
+
+    int multiply (int otherNumber) {
+        return this.number * otherNumber;
+    }
+}
