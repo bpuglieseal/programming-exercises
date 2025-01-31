fcdfcfbe67d44b42de0ed86f14c809c89eddb7fd
Baldassare Pugliese <bpuglieseal24dw@ikzubirimanteo.com>
2025-01-31 14:00:08 +0100

    Avanzando en el ejercicio 3

--- DynamicDataStructures/src/Excercise3/Cinema.java
+++ DynamicDataStructures/src/Excercise3/Cinema.java
@@ -2,6 +2,7 @@ package Excercise3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Cinema {
     private String name;
@@ -46,7 +47,7 @@ public class Cinema {
             return;
         }
 
-        if (fill == limit) {
+        if (this.fill == this.limit) {
             return;
         }
 
@@ -57,25 +58,19 @@ public class Cinema {
     }
 
     public void addMovie(Movie movie) {
-        if (this.movies.contains(movie)) {
-            return;
+        if (!(this.movies.contains(movie)) && !(this.fill == this.limit)) {
+            this.movies.add(movie);
+            int index = this.movies.indexOf(movie);
+
+            while (this.rooms[this.current] instanceof Integer) {
+                if (this.current == this.limit) this.current = 0;
+                else this.current++;
+            };
+
+            this.rooms[this.current] = index;
+            this.fill++;
+            this.current++;
         }
-
-        if (fill == limit) {
-            return;
-        }
-
-        this.movies.add(movie);
-        int index = this.movies.indexOf(movie);
-
-        while (this.rooms[current] instanceof Integer) {
-            if (this.current == limit) this.current = 0;
-            else this.current++;
-        };
-
-        this.rooms[current] = index;
-        this.fill++;
-        this.current++;
     }
 
     public void removeMovie(String movieName) {
@@ -93,6 +88,26 @@ public class Cinema {
         }
     }
 
+    public ArrayList<Movie> moviesShorterThan (int minutes) {
+        ArrayList<Movie> shortherMovies = new ArrayList<Movie>();
+        for (Movie movie : this.movies) {
+            if (movie.getDuration() < minutes) {
+                shortherMovies.add(movie);
+            }
+        }
+        return shortherMovies;
+    }
+
+    public ArrayList<Integer> freeRooms () {
+        ArrayList<Integer> freeRooms = new ArrayList<Integer>();
+        for (int i = 0; i < this.limit; i++) {
+            if (this.rooms[i] instanceof String) {
+                freeRooms.add(i);
+            }
+        }
+        return freeRooms;
+    }
+
     public void display () {
         for (int i = 0; i < this.limit; i++) {
             if (this.rooms[i] instanceof Integer) {
@@ -107,5 +122,41 @@ public class Cinema {
         this.rooms = new Object[this.rooms.length];
 
         Arrays.fill(this.rooms, "?????");
+    }
+
+    public void changeRoom (Movie movie) {
+        if (!this.movies.contains(movie)) {
+            System.out.println("La pelicula no fue encontrada.");
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int newRoom = -1;
+        int movieIndex = this.movies.indexOf(movie);
+        int roomIndex = -1;
+        boolean isValid = false;
+
+        for (int i = 0; i < this.limit && roomIndex == -1; i++) {
+            if (this.rooms[i] instanceof Integer && ((Integer) this.rooms[i]).compareTo(movieIndex) == 0) {
+                roomIndex = i;
+            }
+        }
+
+        System.out.format("- Room de la pelicula: Room - %d\n", roomIndex + 1);
+        System.out.println("+ Salas libres: ");
+
+        for (int i = 0; i < this.limit; i++) {
+            if (this.rooms[i] instanceof String) {
+                System.out.format("- Room %d\n", i + 1);
+            }
+        }
+
+        do {
+            System.out.print("Escoge la nueva sala de la pelicula: ");
+            newRoom = sc.nextInt();
+            isValid = newRoom - 1 < this.limit && newRoom >= 0 && (this.rooms[this.limit - 1] instanceof String);
+        } while (!isValid);
+
+        this.rooms[roomIndex] = "?????";
+        this.rooms[newRoom - 1] = movieIndex;
     }
 }
--- DynamicDataStructures/src/Excercise3/Main.java
+++ DynamicDataStructures/src/Excercise3/Main.java
@@ -1,5 +1,7 @@
 package Excercise3;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Movie thor = new Movie("Thor", 112, 2007);
@@ -7,11 +9,17 @@ public class Main {
         Movie terminator = new Movie("Terminator", 120, 2002);
         Movie superman = new Movie("Superman", 120, 2004);
         Cinema cinema = new Cinema("Super Cinema", 10);
+        Movie avengers = new Movie("Avengers", 146, 2004);
+        ArrayList<Movie> shortherMoviesThan120;
 
         cinema.addMovie(terminator, 1);
         cinema.addMovie(superman, 2);
         cinema.addMovie(spiderman, 3);
         cinema.addMovie(thor);
+        cinema.addMovie(avengers);
+
+        cinema.display();
+        cinema.changeRoom(avengers);
         cinema.display();
     }
 }
