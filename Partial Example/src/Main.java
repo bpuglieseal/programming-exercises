cfc67aa2f31aac88c2624f711c6459aba60246f3
DW31 TALDEA <ik012982i9@H012982.NET>
2025-02-04 13:03:01 +0100

    Avanzando en el ejercicio tipo parcial

--- Partial Example/src/Main.java
+++ Partial Example/src/Main.java
@@ -7,34 +7,82 @@ public class Main {
 
         switch (option) {
             case 1: {
-                System.out.format("+ Drivers of the team %s: ", team);
+                System.out.format("+ Drivers of the team %s:\n", team);
                 workers.forEach(worker -> {
                     if (worker.getType() == WorkerType.DRIVER) {
-                        System.out.format("- %s - %s", worker.getName(), Worker.getVerboseWorkerType(worker.getType()));
+                        System.out.format("- %s - %s\n", worker.getName(), Worker.getVerboseWorkerType(worker.getType()));
                     }
                 });
+                break;
             }
             case 2: {
-                System.out.format("+ Manager of the team %s: ", team);
+                System.out.format("+ Manager of the team %s:\n", team);
                 workers.forEach(worker -> {
                     if (worker.getType() == WorkerType.MANAGER) {
-                        System.out.format("- %s - %s", worker.getName(), Worker.getVerboseWorkerType(worker.getType()));
+                        System.out.format("- %s - %s\n", worker.getName(), Worker.getVerboseWorkerType(worker.getType()));
                     }
                 });
+                break;
             }
             case 3: {
                 workers.forEach(worker -> {
-                    System.out.format("+ Engineers of the team %s: ", team);
+                    System.out.format("+ Engineers of the team %s:\n", team);
                     if (worker.getType() == WorkerType.ENGINEER) {
-                        System.out.format("- %s - %s", worker.getName(), Worker.getVerboseWorkerType(worker.getType()));
+                        System.out.format("- %s - %s\n", worker.getName(), Worker.getVerboseWorkerType(worker.getType()));
                     }
                 });
+                break;
             }
         }
     }
 
-    public static void addNewTeam () {
+    public static void addNewTeam (String newTeam, HashMap<String, ArrayList<Worker>> teams) {
+        teams.put(newTeam, new ArrayList<Worker>());
+    }
 
+    public static void removeTeam (String teamToRemove, HashMap<String, ArrayList<Worker>> teams) {
+        teams.remove(teamToRemove);
+    }
+
+    public static void addWorker (String team, HashMap<String, ArrayList<Worker>> teams, Worker newWorker) {
+        switch (newWorker.getType()) {
+            case DRIVER -> {
+                int count = 0;
+                ArrayList<Worker> workers = teams.get(team);
+                for (Worker worker : workers) {
+                    if (worker.getType() == WorkerType.DRIVER) {
+                        count++;
+                    }
+                }
+                if (count == 3) {
+                    System.out.format("The team %s already have three drivers\n", team);
+                } else {
+                    System.out.format("Driver added to the team %s\n", team);
+                    workers.add(newWorker);
+                }
+            }
+            case MANAGER -> {
+                boolean hasManager = false;
+                ArrayList<Worker> workers = teams.get(team);
+                Iterator<Worker> it = workers.iterator();
+                while (it.hasNext() && !hasManager) {
+                    if (it.next().getType() == WorkerType.MANAGER) {
+                        hasManager = true;
+                    }
+                }
+                if (hasManager) {
+                    System.out.format("The team %s already has a manager", team);
+                } else {
+                    System.out.format("Manager added to the team %s\n", team);
+                    workers.add(newWorker);
+                }
+            }
+            case ENGINEER -> {
+                ArrayList<Worker> workers = teams.get(team);
+                workers.add(newWorker);
+                System.out.format("Engineer added to the team %s\n", team);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -83,15 +131,61 @@ public class Main {
                     } while (subMenuOption < 1 || subMenuOption > 3);
 
                     showWorkers(teams, subMenuOption, team);
+                    break;
                 }
                 case 2: {
                     System.out.print("Input the name of the new team: ");
                     String team = sc.nextLine();
 
-                    if (!teams.containsKey(team.toLowerCase())) {
+                    if (teams.containsKey(team.toLowerCase())) {
                         System.out.println("- The indicated team already exists");
                         break;
                     }
+
+                    addNewTeam(team, teams);
+                }
+                case 3: {
+                    System.out.print("Input the name of the team you want to remove: ");
+                    String team = sc.nextLine();
+
+                    if (!teams.containsKey(team.toLowerCase())) {
+                        System.out.println("- The indicated team does not exist");
+                        break;
+                    }
+
+                    removeTeam(team, teams);
+                }
+                case 4: {
+                    sc.nextLine();
+                    System.out.print("Input the name of the team you want to add a worker: ");
+                    String team = sc.nextLine();
+
+                    if (!teams.containsKey(team.toLowerCase())) {
+                        System.out.println("- The indicated team does not exist");
+                        break;
+                    }
+
+                    int subMenuOption;
+
+                    do {
+                        System.out.print("¿What kind of worker you want to create?: ");
+                        subMenuOption = sc.nextInt();
+                        if (subMenuOption < 1 || subMenuOption > 3) System.out.println("Invalid option. Try Again");
+                    } while (subMenuOption < 1 || subMenuOption > 3);
+
+                    sc.nextLine();
+                    System.out.print("Input the name of your new worker: ");
+                    String name = sc.nextLine();
+                    Worker worker;
+
+                    switch (subMenuOption) {
+                        case 1 -> worker = new Worker(name, WorkerType.DRIVER);
+                        case 2 -> worker = new Worker(name, WorkerType.MANAGER);
+                        default -> worker = new Worker(name, WorkerType.ENGINEER);
+                    }
+
+                    addWorker(team, teams, worker);
+                    break;
                 }
                 case 6: {
                     System.out.println("Bye Bye!");
